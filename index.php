95396965c3837c7d6a4df7057110650c9f5945be
adars <adars.nepal@gmail.com>
2023-03-28 20:14:39 +0100

    Command Pattern addedf

--- index.php
+++ index.php
@@ -8,6 +8,10 @@ include_once ('vendor/autoload.php');
 ## fetched a singleton implementation of application
 try {
     $application = \App\Base\Application::getInstance();
+    if(strtolower($_ENV['lockdown_mode']??"false")=="true")
+    {
+        echo("LOCKDOWN MODE");die;
+    }
     $application::route();
     $application::exitApplication();
 }
--- src/Base/QueryLogger.php
+++ src/Base/QueryLogger.php
@@ -4,11 +4,6 @@ namespace App\Base;
 
 class QueryLogger implements Observer
 {
-    public function __construct()
-    {
-
-    }
-
     public function notify($query)
     {
         file_put_contents(Application::$base_path.'/../../logs/log.txt',$query." \n",FILE_APPEND | LOCK_EX);
